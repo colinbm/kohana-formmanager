d35cc803645174277fe540126ab56e3c2908cacb
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-03-22 06:33:50 -0700

    Add new line after button

--- views/formmanager/html/buttons.php
+++ views/formmanager/html/buttons.php
@@ -1,7 +1,7 @@
 <div class="form-actions">
 <?php
 foreach($buttons as $button) {
-	echo Form::button($button['name'], $button['text'], $button['attributes']);
+	echo Form::button($button['name'], $button['text'], $button['attributes']), "\n";
 }
 ?>
 </div>
