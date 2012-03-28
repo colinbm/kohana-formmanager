7bf43428bca2b2d29693f0c25e6292c79761585e
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-03-28 03:16:03 -0700

    Use field_name not column_name for field name, as column_name is only present in model forms.

--- views/formmanager/html/hidden.php
+++ views/formmanager/html/hidden.php
@@ -1,1 +1,1 @@
-<input type="hidden" name="<?php echo $field['column_name']; ?>" value="<?php echo $field['value']; ?>" />
+<input type="hidden" name="<?php echo $field['field_name']; ?>" value="<?php echo $field['value']; ?>" />
