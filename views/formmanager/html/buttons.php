e5f37d747b2679d698e350c757c3c068c4ea7598
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-10-22 16:01:42 +0200

    Use input instead of button, as it should respect the type attribute.

--- views/formmanager/html/buttons.php
+++ views/formmanager/html/buttons.php
@@ -1,7 +1,7 @@
 <div class="form-actions">
 <?php
 foreach($buttons as $button) {
-	echo Form::button($button['name'], $button['text'], $button['attributes']), "\n";
+	echo Form::input($button['name'], $button['text'], $button['attributes']), "\n";
 }
 ?>
 </div>
